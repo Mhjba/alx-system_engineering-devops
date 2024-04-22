380742f1754a06d4cbb1164ab15ae022cd79c39a
Mhjba <mahjoubaaitalla15@gmail.com>
2024-04-22 03:40:52 -0700

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,18 +4,18 @@ TODO list progress with his/her employee ID
 """
 
 if __name__ == "__main__":
-        import requests
-            from sys import argv
+    import requests
+    from sys import argv
 
-                uri = f'https://jsonplaceholder.typicode.com/users/{argv[1]}/'
-                    u_data = requests.get(uri).json()
-                        td_data = requests.get(uri + 'todos')
+    uri = f'https://jsonplaceholder.typicode.com/users/{argv[1]}/'
+    u_data = requests.get(uri).json()
+    td_data = requests.get(uri + 'todos')
 
-                            td_completed = [td for td in td_data.json() if td.get('completed') is True]
+    td_completed = [td for td in td_data.json() if td.get('completed') is True]
 
-                                dn = len(td_completed)
-                                    td = len(td_data.json())
+    dn = len(td_completed)
+    td = len(td_data.json())
 
-                                        print(f"Employee {u_data.get('name')} is done with tasks({dn}/{td}):")
-                                            for i in td_data.json():
-                                                        print(f"\t {i.get('title')}")
+    print(f"Employee {u_data.get('name')} is done with tasks({dn}/{td}):")
+    for i in td_data.json():
+        print(f"\t {i.get('title')}")
