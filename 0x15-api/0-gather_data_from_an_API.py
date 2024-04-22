3dd5db122c4203125b83bb6c3e7f3901a21baac7
Mhjba <mahjoubaaitalla15@gmail.com>
2024-04-22 04:14:47 -0700

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     us_data = requests.get(url + "users/{}".format(argv[1])).json()
     td_data = requests.get(url + "todos", params={"userId": argv[1]}).json()
 
-    comp = [tl.get("title") for tl in td_data  if tl.get("completed") is True]
+    comp = [tl.get("title") for tl in td_data if tl.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
-        us_data.get("name"), len(comp), len(td_data )))
+        us_data.get("name"), len(comp), len(td_data)))
     [print("\t {}".format(c)) for c in comp]
