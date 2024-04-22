ef6c296a0b11d9d1d73d7b8b2e87a33815eaf5e3
Mhjba <mahjoubaaitalla15@gmail.com>
2024-04-22 04:10:25 -0700

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
-import requests
-import sys
+"""gather data from an api"""
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    import requests
+    from sys import argv
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    url = "https://jsonplaceholder.typicode.com/"
+    us_data = requests.get(url + "users/{}".format(argv[1])).json()
+    td_data = requests.get(url + "todos", params={"userId": argv[1]}).json()
+
+    comp = [tl.get("title") for tl in td_data  if tl.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+        us_data.get("name"), len(comp), len(td_data )))
+    [print("\t {}".format(c)) for c in comp]
