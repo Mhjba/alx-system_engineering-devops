e7f998db012c8277d6a923d19f719f2aea74a8d0
Mhjba <mahjoubaaitalla15@gmail.com>
2024-04-22 12:27:02 +0200

    api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""gather data from an api"""
+import requests
+import sys
+
+from requests import get
+from sys import argv
+
+if __name__ == "__main__":
+        url = get("https://jsonplaceholder.typicode.com/")
+            user = requests.get(url + "users/{}".format(argv[1])).json()
+                todos = requests.get(url + "todos", params={"userId": argv[1]}).json()
+
+                    completed = [i.get("title") for i in todos if i.get("completed") is True]
+                        print("Employee {} is done with tasks({}/{}):".format(
+                                    user.get("name"), len(completed), len(todos)))
+                            [print("\t {}".format(c)) for c in completed]
