1bf1918e8cec1f415e5798bd294a1ff362192a29
Mhjba <mahjoubaaitalla15@gmail.com>
2024-05-07 21:00:26 +0200

    api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,7 +5,7 @@
 def number_of_subscribers(subreddit):
     """Queries the Reddit API and returns the number of subscribers"""
     import requests
-    
+
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
@@ -14,4 +14,5 @@ def number_of_subscribers(subreddit):
         data = response.json()
         subscribers = data['data']['subscribers']
         return 0
-    return subscribers
+    else:
+        return subscribers
