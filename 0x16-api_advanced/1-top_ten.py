db06eab1fa6c4463ed49a75740d85a429bc6fb58
Mhjba <mahjoubaaitalla15@gmail.com>
2024-05-07 21:09:17 +0200

    api advanced

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 ''' return top ten posts for a subreddit '''
-import requests
 
 
 def top_ten(subreddit):
     ''' function to return top 10 posts '''
+    import requests
+
     url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-    headers = {'User-Agent': 'Mozilla/5.0 (by Omar)'}
-    params = {'limit': 9}
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json().get('data')
@@ -17,5 +16,5 @@ def top_ten(subreddit):
 
         for post in hot_posts:
             print(post['data']['title'])
-    else:
-        print(None)
+
+    print(None)
